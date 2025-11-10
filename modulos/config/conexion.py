e47fc456da23ae0fbd4d9cc4807d0222e9acf089
Debbs2410 <00043322@uca.edu.sv>
2025-11-10 14:45:44 -0600

    Update database connection parameters

--- modulos/config/conexion.py
+++ modulos/config/conexion.py
@@ -4,10 +4,10 @@ import streamlit as st
 def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",   # tu contraseña de MySQL
-            database="cooperativa_db"
+            host="bnsf0ymzpvkirhrcgqov-mysql.services.clever-cloud.com",
+            user="uz0ilcx1uwofz9ys",
+            password="Q8EPNcwHwEh7PScgYUkT",   # tu contraseña de MySQL
+            database="bnsf0ymzpvkirhrcgqov"
         )
         return conexion
     except mysql.connector.Error as e:
