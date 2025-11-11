905c214cfa38cf7a940e9a9212d9f7b6f7e968a3
Debbs2410 <00043322@uca.edu.sv>
2025-11-11 15:48:17 -0600

    Update conexion.py

--- modulos/config/conexion.py
+++ modulos/config/conexion.py
@@ -3,13 +3,13 @@ import streamlit as st
 
 def obtener_conexion():
     try:
-        conexion = mysql.connector.connect(
-            host="bnsf0ymzpvkirhrcgqov-mysql.services.clever-cloud.com",
-            user="uz0ilcx1uwofz9ys",
-            password="Q8EPNcwHwEh7PScgYUkT",   # tu contraseña de MySQL
-            database="bnsf0ymzpvkirhrcgqov"
+        return mysql.connector.connect(
+            host="bnsf0ymzpvkirhrcgqov-mysql.services.clever-cloud.com",        # o el nombre de tu servidor
+            user="uz0ilcx1uwofz9ys",             # tu usuario MySQL
+            password="Q8EPNcwHwEh7PScgYUkT",             # tu contraseña MySQL
+            database="bnsf0ymzpvkirhrcgqov"   # nombre de tu base de datos
         )
-        return conexion
     except mysql.connector.Error as e:
         st.error(f"Error de conexión: {e}")
         return None
+
