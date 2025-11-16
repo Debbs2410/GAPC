2d92aa6f72f1297c32f4eb4b63cfd3f6f9074783
00067922-spec <00067922@uca.edu.sv>
2025-11-15 23:11:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from modulos.login import login
 from modulos.panel import mostrar_panel
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'modulos'))
+
 st.set_page_config(page_title="Cooperativa GAPC", layout="wide")
 
 if "autenticado" not in st.session_state:
