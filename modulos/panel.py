8c88a38c1b6ba8ba5a34c28c694885c89d202723
00067922-spec <00067922@uca.edu.sv>
2025-11-15 23:22:59 -0600

    Update panel.py

--- modulos/panel.py
+++ modulos/panel.py
@@ -1,7 +1,9 @@
 def mostrar_panel():
     import streamlit as st
     
-    # Esta línea ahora debe funcionar porque la ruta fue corregida en app.py
+    from modulos.registro_beneficiarios import registrar_beneficiario, ver_todos_miembros, crear_miembro
+    from modulos.registro_usuarios import registrar_usuario
+    
    
     # ... el resto de la función ...
     
