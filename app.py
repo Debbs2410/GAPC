1300eb1a9a65244841998becaa1cbfcdae37b3a9
00067922-spec <00067922@uca.edu.sv>
2025-11-15 23:15:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 from modulos.login import login
 from modulos.panel import mostrar_panel
+# --- EN EL ARCHIVO app.py ---
+
+import sys
+import os
+
+# Obtiene la ruta del directorio de trabajo actual
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Agrega la carpeta 'modulos' al camino de búsqueda de Python
+# Esto permite que las importaciones relativas y absolutas funcionen correctamente.
+sys.path.append(os.path.join(current_dir, 'modulos'))
+
+# ... El resto de tu código de app.py continúa aquí ...
 
 st.set_page_config(page_title="Cooperativa GAPC", layout="wide")
 
